a5e16669da6b0b6400d7cbdf643e87ad5ce62ddd
Patrick Aliberti <56771699+PatrickAliberti@users.noreply.github.com>
2021-05-18 11:10:18 -0400

    Add files via upload

---
+++ test.js
@@ -0,0 +1,6 @@
+import { type } from 'os';
+
+document.getElementById("output").innerHTML = type();
+/*console.log('Platform: '+os.platform());
+console.log('Total Memory: '+os.totalmem()+' bytes');
+console.log('Free Memory: '+os.freemem()+' bytes');*/
